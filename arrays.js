69a790bb396801cd0be2438db4dda41a76af6201
CR7tiantian <1508108789@qq.com>
2016-02-02 11:12:20 +0800

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,19 +1,23 @@
 (function() {
 	// every() 返回的是个boolean值
-	var numbers = [1,2,3,4,5,4,3,2,1];
-	var every = numbers.every(function(item,index,array) {
-		return (item > 3);
-	})
-	// some()  返回的是个boolean值
-	var some = numbers.some(function(item,index,array){
-		return (item >3);
+	var numbers = [1, 2, 3, 4, 5, 4, 3, 2, 1];
+	var every = numbers.every(function(item, index, array) {
+			return (item > 3); //false
+		})
+		// some()  返回的是个boolean值
+	var some = numbers.some(function(item, index, array) {
+		return (item > 3); //true
 	});
 	// filter()  返回的是一个数组
-	var filter = numbers.filter(function(item,index,array){
-		return (item<4);
+	var filter = numbers.filter(function(item, index, array) {
+		return (item < 4); //[1,2,3,3,2,1]
 	});
 	// map()  返回的是一个数组
-	var map = numbers.map(function(item,index,array){
-		return (item*3);
+	var map = numbers.map(function(item, index, array) {
+		return (item * 3); //[3,6,9,12,15,12,9,6,3]
+	});
+	// reduce() 
+	var reduce = numbers.reduce(function(prev, cur, index, array) {
+		return (prev * cur); //120*24
 	});
 })();
